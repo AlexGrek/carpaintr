54120d60f6c5aebb2fc77039ae4384a848828d28
alexgrek <alexgrek.hq@gmail.com>
2025-05-21 17:49:17 -0400

    rmoved old backends

--- backend/internal/models/company.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// CompanyInfo represents comprehensive company information
-type CompanyInfo struct {
-	Email       string       `json:"email"`
-	License     *LicenseInfo `json:"license"`
-	CompanyName string       `json:"company_name"`
-	CurrentTime string       `json:"current_time"`
-}
--- backend/internal/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// User represents a user in the system
-type User struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
