84b1ba577fbfc1e9957ee01d32fc0e5fb84f1e01
alexgrek <alexgrek.hq@gmail.com>
2025-06-25 22:58:30 +0300

    color grid update

--- carpaintr-front/src/components/ColorGrid.jsx
+++ carpaintr-front/src/components/ColorGrid.jsx
@@ -4,23 +4,38 @@ import Trans from '../localization/Trans';
 import { useLocale } from '../localization/LocaleContext';
 
 const ColorGrid = ({ colors, selectedColor, onChange }) => {
-  return (
-    <div className="color-grid-container">
-      {colors.map(({ hex, name_eng, name_ukr }) => (
-        <div
+  const renderColor = (color) => {
+    const { hex, name_eng, name_ukr } = color;
+    return <div
           key={name_ukr}
-          className={`color-card ${selectedColor === name_ukr ? "selected-card" : ""}`}
-          onClick={() => onChange(name_ukr)}
+          className={`color-card fade-in-simple ${selectedColor === name_ukr ? "selected-card" : ""}`}
+          onClick={() => onChange(selectedColor === name_ukr ? null : name_ukr)}
         >
           <div
             className="color-preview"
             style={{ backgroundColor: hex }}
-          ></div>
+          >
+
+          </div>
           <div className="label">
             <span>{name_ukr}</span>
           </div>
         </div>
-      ))}
+  }
+
+  const renderSelectedColor = () => {
+    const color = colors.find((item) => item.name_ukr === selectedColor);
+    if (color)
+      return renderColor(color);
+    else {
+      console.error(color);
+      return <p>{JSON.stringify(color)}</p>
+    }
+  }
+
+  return (
+    <div className={selectedColor == null ? "color-grid-container" : "color-grid-container-selected"}>
+      {selectedColor == null ? colors.map(renderColor) : renderSelectedColor()}
     </div>
   );
 };
