944264d29ee840cf894ee28b505583a03a24029b
Alex Grek <alexgrek.hq@gmail.com>
2025-09-01 23:12:20 +0300

    PWA

--- carpaintr-front/vite.config.js
+++ carpaintr-front/vite.config.js
@@ -3,12 +3,34 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { writeFileSync } from 'fs';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
-const appVersion = new Date().toISOString(); 
+const appVersion = new Date().toISOString();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    manifest: {
+      name: 'Autolab',
+      short_name: 'Autolab',
+      description: 'Your app description here',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
   build: {
     rollupOptions: {
       plugins: [
